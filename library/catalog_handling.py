fixed snr calculation

library/catalog_handling.py
@@ -38,9 +38,9 @@ def process_catalog_batches(catalog, T_obs, delta_t, tdi,  batch_size, output_fi
     
     # Create different N values to assign depending on the required N (depends on the fdot)
     N_values = {
-    "small": 64,
-    "medium": 256,
-    "large": 4096
+    "small": 128,
+    "medium": 512,
+    "large": 2048
     }
     bucket_items = list(N_values.items())
 
@@ -113,9 +113,9 @@ def process_catalog_batches(catalog, T_obs, delta_t, tdi,  batch_size, output_fi
 
             bucket = np.full(len(f0),'skip', dtype='U10') # initialize the bukcets with all skip
 
-            bucket[(required_bins < 1e3) & loud_sources_mask] = "small"
-            bucket[(required_bins >= 1e3) & (required_bins < 1e4) & loud_sources_mask] = "medium"
-            bucket[(required_bins >= 1e4) & loud_sources_mask] = "large"
+            bucket[(required_bins < 1e2) & loud_sources_mask] = "small"
+            bucket[(required_bins >= 1e2) & (required_bins < 1e3) & loud_sources_mask] = "medium"
+            bucket[(required_bins >= 1e3) & loud_sources_mask] = "large"
 
             # process each bucket individually
             for bname, N in N_values.items():

library/iteration_utils.py
@@ -199,12 +199,12 @@ def confusion_psd_from_signal(A_tot, df,  filter_size=1000):
 
     Parameters:
 
-    A_tot: total signal 
+    A_tot: total signal of  channel A=channel E 
     df: frequency resolution (1/T_obs)
     filter_size: number of bins for the smoothing 
 
     """
-    signal_psd = 2.0 * df * np.abs(A_tot)**2
+    signal_psd = 2.0 * df * np.abs(A_tot**2)
     #norm = 1.0 / 0.7023319615912207
     psd_conf = ndimage.median_filter(signal_psd, size=filter_size) 
 
@@ -378,12 +378,10 @@ def run_iterative_separation(state,
             A = state['source_A'][idx]
             i0, i1 = state['source_idx_ranges'][idx]
             A_tot[i0:i1] += A
-            E = state['source_E'][idx]
-            i0, i1 = state['source_idx_ranges'][idx]
-            E_tot[i0:i1] += E
+
 
         psd_confusion= confusion_psd_from_signal(
-            A_tot=(A_tot**2 + E_tot**2)**(1/2),
+            A_tot=A_tot,
             df=state['df'],
             filter_size=filter_size
         )